4b542e62153702c6aae38e58acd41bd1fd85d79c
Alexis Serapio <alexis.serapioh@gmail.com>
2025-08-13 22:31:47 -0600

    Se añade un Container principal para el despliegue del nombre de la receta y el autor

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, title: 'Hola Mundo', home: RecipeBook()); //Container(child: Text('Hola Mundo'),)
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola Mundo',
+      home: RecipeBook(),
+    ); //Container(child: Text('Hola Mundo'),)
   }
 }
 
@@ -16,6 +20,30 @@ class RecipeBook extends StatelessWidget {
   //Constructor y lo que nos va a enseñar todo lo que tengamos en return
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: Colors.deepOrange, title: Text('Libro de Recetas', style: TextStyle(color: Colors.white),),),); //Es una hoja de libro en blanco
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('Libro de Recetas', style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(width: MediaQuery.of(context).size.width, height: 125, child: Card(child: Row(children: <Widget>[
+        Container(
+          height: 125,
+          width: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(),
+          ),
+        ),
+        SizedBox(width: 26,),
+        Column(children: <Widget>[
+          Text('Lasagna'),
+          Text('Alexis Serapio'),
+          Container(
+            height: 2,
+            width: 75,
+            color: Colors.red)
+        ],)
+      ],),),),
+    ); //Es una hoja de libro en blanco
   }
 }
